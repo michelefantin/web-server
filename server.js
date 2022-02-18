315a29c1969c431310fce5f0e9a44cb53d21023e
Michele Fantin <michele.fantin@stud.tecnicosuperiorekennedy.it>
2022-02-18 14:59:08 +0100

    Deleted some comments

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ var PORT = 3000;
 
 var middleware = require('./middleware.js')
 
-
-
 app.use(middleware.logger);
- //app.use(middleware.requireAuthentication);
-
-// app.get('/', function (req,res) {
-//     res.send('Hello Express!')
-// });
 
 app.get('/about', middleware.requireAuthentication, function(req,res){
     res.send('About us!')
